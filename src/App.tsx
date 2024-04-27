79a96525de586fc931536c6c463311fa52c5b76a
Long Pham <phamquanglong.hcm@gmail.com>
2024-04-27 17:01:48 +0700

    add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import { Amplify } from 'aws-amplify';
 import type { WithAuthenticatorProps } from '@aws-amplify/ui-react';
-import { withAuthenticator } from '@aws-amplify/ui-react';
+import { withAuthenticator, ThemeProvider, Heading, Button } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-
+import './App.css'
 import awsconfig from './aws-exports';
-import { ToDoListCreateForm } from './ui-components';
+import { ToDoListCreateForm, ToDoListUpdateForm } from './ui-components';
+import { myTheme } from './assets/theme';
 Amplify.configure(awsconfig);
 
 export function App({ signOut, user }: WithAuthenticatorProps) {
-  console.log(user)
   return (
-    <>
-      <div className='App'>
-        <h1>Hello {user?.signInDetails?.loginId}</h1>
-        <button onClick={signOut}>Sign out</button>
-      </div>
-      <div className='container1'>
-        <div className='content1'>
-          <ToDoListCreateForm></ToDoListCreateForm>
+    <ThemeProvider theme={myTheme}>
+      <div className="App">
+        <div className='heading-top'>
+          <Heading level={4}>Hello {user?.signInDetails?.loginId}</Heading>
+          <Button variation="link" size="small"
+          onClick={signOut}>Sign out</Button>
+        </div>
+        <div className='container'>
+          <div className='content'>
+          </div>
         </div>
       </div>
-    </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/assets/theme.ts
@@ -0,0 +1,12 @@
+import { Theme } from "@aws-amplify/ui-react";
+
+export const myTheme: Theme = {
+    name: 'my-theme',
+    tokens: {
+      colors: {
+        font: {
+          primary: { value: 'red' },
+        },
+      },
+    },
+  };
