9489f722c8ff01a4ba2b6a8d07ddae07815d3d74
Long Pham <phamquanglong.hcm@gmail.com>
2024-04-27 19:35:35 +0700

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,30 @@
 import { Amplify } from 'aws-amplify';
 import type { WithAuthenticatorProps } from '@aws-amplify/ui-react';
-import { withAuthenticator, ThemeProvider, Heading, Button } from '@aws-amplify/ui-react';
+import { withAuthenticator, Label, Button, Text, Image } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import './App.css'
 import awsconfig from './aws-exports';
-import { ToDoListCreateForm, ToDoListUpdateForm } from './ui-components';
-import { myTheme } from './assets/theme';
 Amplify.configure(awsconfig);
 
 export function App({ signOut, user }: WithAuthenticatorProps) {
-  return (
-    <ThemeProvider theme={myTheme}>
-      <div className="App">
-        <div className='heading-top'>
-          <Heading level={4}>Hello {user?.signInDetails?.loginId}</Heading>
-          <Button variation="link" size="small"
-          onClick={signOut}>Sign out</Button>
-        </div>
-        <div className='container'>
-          <div className='content'>
-          </div>
-        </div>
-      </div>
-    </ThemeProvider>
-  );
+  return (
+      <div className="App">
+        <div className='heading-top'>
+            <Image  
+                  width="82px"
+                  src="/src/assets/logo.png" 
+                  alt="Notethings logo">
+            </Image>
+          <Label color="white" htmlFor="hi">Hi <Text  lineHeight="1.5em" as="span" fontSize="1.1em" isTruncated={true} variation="success">{user?.signInDetails?.loginId}</Text></Label>
+          <Button variation="primary"
+          onClick={signOut}>Sign out</Button>
+        </div>
+        <div className='container'>
+          <div className='content'>
+          </div>
+        </div>
+      </div>
+  );
 }
 
 export default withAuthenticator(App);
